bf0be21d0b6a4076061be3b4345f05387421ee0c
Varsha <varshakt6@gmail.com>
2023-07-19 19:19:16 +0530

    dataingestion

--- src/exception.py
+++ src/exception.py
@@ -18,8 +18,9 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
-    if __name__=="__main__":
+
+
+if __name__=="__main__":
         try:
             a=1/0
         except Exception as e:
