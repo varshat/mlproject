cd0e3a1685b7cc4e502319057fea3e3bcbceaa53
Varsha <varshakt6@gmail.com>
2023-07-19 23:06:04 +0530

    Data Transformation code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -58,3 +58,11 @@ if __name__=="__main__":
 
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+# if __name__=="__main__":
+#     obj=DataIngestion()
+#     train_data,test_data=obj.initiate_data_ingestion()
+
+#     data_transformation=DataTransformation()
+#     data_transformation.initiate_data_transformation()
